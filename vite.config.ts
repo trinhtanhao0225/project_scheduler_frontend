b1d2c4c097fd8e1d9a568892fe89be7abb53adcb
trinhtanhao0225 <145914412+trinhtanhao0225@users.noreply.github.com>
2026-04-08 14:42:34 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,26 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import path from 'path'
 
-// Nếu bạn dùng CommonJS style, file này vẫn chạy trên Vite 8+
-
 export default defineConfig({
   plugins: [
-    react(),         // Plugin React
-    tsconfigPaths(), // Cho phép đọc alias từ tsconfig
+    react(),
+    tsconfigPaths(),
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // alias @ => src
+      '@': path.resolve(__dirname, 'src'),
     },
   },
   server: {
-  proxy: {
-    '/api': {
-      target: 'http://140.115.59.61:8888',
-      changeOrigin: true,
-      secure: false,
-      rewrite: (path) => path.replace(/^\/api/, ''),   // bỏ /api trước khi gửi sang backend
+    proxy: {
+      '/api': {
+        target: 'http://140.115.59.61:8888',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
     },
   },
-},
 })
